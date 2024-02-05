2c4067ed467dd249360a2767eb7ed2da345620fe
ezzaytouni othmane <ezzaytouniothmane@gmail.com>
2024-02-05 03:35:08 -0800

    inhe

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,16 +1,21 @@
- a Rectangle subclass Square."""
-Rectangle = __import__('9-rectangle').Rectangle
+#!/usr/bin/python3
+"""
+This program improves a previous Square
+"""
 
 
-class Square(Rectangle):
-    """Represent a square."""
+PrevSquare = __import__('10-square').Square
+
+
+class Square(PrevSquare):
+    """
+    Improved Class Square
+    """
 
     def __init__(self, size):
-        """Initialize a new square.
+        """Constructor of the improved Square"""
+        super().__init__(size)
 
-        Args:
-            size (int): The size of the new square.
-        """
-        self.integer_validator("size", size)
-        super().__init__(size, size)
-        self.__size = size
+    def __str__(self):
+        """String representation of Square"""
+        return '[Square] {0:d}/{0:d}'.format(self.__size)
